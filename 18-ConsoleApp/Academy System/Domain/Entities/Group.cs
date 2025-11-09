2e9c261b18cdf431c88cf11c3ee382d4aa435c95
Nuriyeva Rahide <rahideshn-bpa203@code.edu.az>
2025-11-09 22:18:29 +0400

    Console App

---
+++ 18-ConsoleApp/Academy System/Domain/Entities/Group.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Group : BaseEntity
+    {
+       
+        public string Teacher { get; set; }
+        public string Room { get; set; }
+        
+    }
+}
---
+++ 18-ConsoleApp/Academy System/Service/Services/Implementations/GroupService.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Repository.Repositories.Implementations;
+using Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.Implementations
+{
+    public class GroupService : IGroupService
+    {
+        private GroupRepository _groupRepository;
+        private int _count;
+        public Group Create(Group group)
+        {
+            group.Id = _count; 
+            _groupRepository.Create(group);
+            _count++;
+            return group;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Update(int id, Group group)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
